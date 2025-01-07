c02a36e028f7e3ea2bc16de89eafb7badc3775d8
an1604 <nataf12386@cs.colman.ac.il>
2025-01-07 10:31:56 +0200

    continuation

--- portofolio/components/Works.jsx
+++ portofolio/components/Works.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import Image from 'next/image';
-import { assets, workData } from '@/assets/assets';
+import {assets, workData} from '@/assets/assets';
 
 const Works = () => {
-  return (
-    <div id='works' className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
-      <h2 className='text-center text-5xl font-Ovo'>My latest works</h2>
-      <p className='flex justify-center items-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-        Explore my latest works and projects in this section!
-        <a 
-          href='https://github.com/an1604' 
-          target='_blank' 
-          rel='noopener noreferrer' 
-          className='flex items-center gap-2 ml-2'
-        >
-          <span>Click the Icon</span>
-          <Image 
-            src={assets.github} 
-            alt='GitHub' 
-            width={40} 
-            height={40} 
-            className='cursor-pointer hover:opacity-80' 
-          />
-        </a>
-      </p>
+    return (
+        <div id='works' className='w-full px-[12%] py-10 scroll-mt-20'>
+            <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
+            <h2 className='text-center text-5xl font-Ovo'>My latest works</h2>
+            <p className='flex justify-center items-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                Explore my latest works and projects in this section!
+                <a
+                    href='https://github.com/an1604'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='flex items-center gap-2 ml-2'
+                >
+                    <span>Click the Icon</span>
+                    <Image
+                        src={assets.github}
+                        alt='GitHub'
+                        width={40}
+                        height={40}
+                        className='cursor-pointer hover:opacity-80'
+                    />
+                </a>
+            </p>
 
-        <div>
-            {workData.map((prject, index)=>(
-                <div key={index} style={{backgroundImage: `url(${prject.bgImage})`}}>
-                    
-                </div>
-            ))}
+            <div className="grid grid-cols-auto my-10">
+                {workData.map((project, index) => (
+                    <div key={index} style={{backgroundImage: `url(${project.bgImage})`}}>
+                        <div>
+                            <h2>{project.title}</h2>
+                            <p>{project.description}</p>
+                        </div>
+                        <div>
+                            <Image src={assets.send_icon} alt='send icon' className='w-5'/>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
-
-    </div>
-  );
+    );
 };
 
 export default Works;
