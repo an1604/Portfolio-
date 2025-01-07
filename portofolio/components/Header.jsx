a6b724e1bc5fcec18c118f13787750eb131d0bda
an1604 <nataf12386@cs.colman.ac.il>
2025-01-07 17:30:00 +0200

    did more animations

--- portofolio/components/Header.jsx
+++ portofolio/components/Header.jsx
@@ -15,35 +15,53 @@ const Header = () => {
         <Image src={assets.profile_img} alt="Profile picture of Aviv Nataf" className="rounded-full w-32" />
       </motion.div>
 
-      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+      <motion.h3 
+      initial={{y:-20, opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration:0.6, delay:0.3 }}
+      className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
         Hi! I'm Aviv Nataf <Image src={assets.hand_icon} alt="Waving hand icon" className="w-6" />
-      </h3>
+      </motion.h3>
 
-      <h1 className="text-xl sm:text-4xl lg:text-5xl font-Ovo">
+      <motion.h1 
+      initial={{y:-30, opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration:0.8, delay:0.5 }}
+      className="text-xl sm:text-4xl lg:text-5xl font-Ovo">
             Junior Developer / Cybersecurity Enthusiast.
-     </h1>
+     </motion.h1>
 
 
-      <p className="max-w-2xl mx-auto font-Ovo">
+      <motion.p
+      initial={{opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration:0.6, delay:0.7 }}
+      className="max-w-2xl mx-auto font-Ovo">
         Aspiring developer skilled in Python, Java, full-stack, AI, and cybersecurity, passionate about innovative, impactful solutions.
-      </p>
+      </motion.p>
 
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-        <a
+        <motion.a
+        initial={{y:30, opacity:0}}
+        whileInView={{y:30,opacity:1}}
+        transition={{duration:0.6, delay:1 }}
           href="#contact"
           className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2
           dark:bg-transparent"
         >
           Contact me <Image src={assets.right_arrow_white} alt="Right arrow icon" className="w-4" />
-        </a>
+        </motion.a>
 
-        <a
+        <motion.a
+        initial={{y:30, opacity:0}}
+        whileInView={{y:30,opacity:1}}
+        transition={{duration:0.6, delay:1.2 }}
           href="/aviv_nataf_resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
           My resume <Image src={assets.download_icon} alt="Download icon" className="w-4" />
-        </a>
+        </motion.a>
       </div>
     </div>
   );
