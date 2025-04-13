552d5433853e364233d566b3e1606cc513ea0cde
Aviv Yosef Nataf <118442152+an1604@users.noreply.github.com>
2025-04-13 17:42:48 +0300

    Update deploy_docker.sh

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 docker build -t portofolio .
-docker run -p 3000:3001 portofolio
+
+docker stop $(docker ps -q --filter ancestor=portofolio) || true
+docker rm $(docker ps -a -q --filter ancestor=portofolio) || true
+
+docker run -d -p 3000:3001 portofolio
